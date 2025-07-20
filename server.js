ad5c31f99b0fca8f95cfe69c52214f28b9fe57df
GabrielForestiero <gabyforestiero@gmail.com>
2025-07-20 12:54:43 -0300

    Initial commit

--- server.js
+++ server.js
@@ -3,9 +3,6 @@ import dotenv from 'dotenv'
 import cors from 'cors'
 
 import authRoutes from './src/routes/auth.routes.js'
-import productRoutes from './src/routes/product.routes.js'
-import cartRoutes from './src/routes/cart.routes.js'
-import orderRoutes from './src/routes/order.routes.js'
 import userRoutes from './src/routes/user.routes.js'
 import { connectDB } from './src/config/db.js'
 
@@ -23,9 +20,6 @@ app.use(cors({
 app.use(express.json())
 
 app.use('/api/auth', authRoutes)
-app.use('/api/product', productRoutes)
-app.use('/api/cart', cartRoutes)
-app.use('/api/order', orderRoutes)
 app.use('/api/user', userRoutes)
 
 const PORT = process.env.PORT || 3000
